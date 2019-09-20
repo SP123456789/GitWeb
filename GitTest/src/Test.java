01b491aba8479f2d6bab32dea227f59e9bb89dc4
SP123456789 <15530210373@163.com>
2019-09-20 16:44:50 +0800

    ver 2.0

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Learn Git and GitHub without any code!");
+		System.out.println("Learn Git and GitHub without any code!");
 	}
 }
